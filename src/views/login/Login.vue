6e14714ff8cd6dab0d33c082b07c2c2685afcbe7
李汶龙 <liwenlong@charmacy.com>
2021-01-20 16:04:27 +0800

    修改备案号

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -45,14 +45,14 @@
                     </div>
                 </div>
             </div>
-            <!-- <div class="icpText">
+            <div class="icpText">
                 <a
                     target="_blank"
-                    href="http://www.beian.miit.gov.cn/"
+                    href="http://beian.miit.gov.cn/"
                     style="color: #606266;margin-bottom:10px;"
-                >粤ICP备05008078号</a>
-                <p style="margin-top:6px;">Copyright © CHARMACY PHARMACEUTICAL CO., LTD.</p>
-            </div> -->
+                >粤ICP备2021000432号</a>
+                <p style="margin-top:6px;">Copyright © 佛山市万泰家具有限公司</p>
+            </div>
         </div>
     </div>
 </template>
